9c874c4e552d72f054eba0493b41a7e7749d3bf6
Abc <pupnejachirag109770@gmail.com>
2024-05-03 11:51:09 +0530

    Edit express and configure actions for self hosted and created API

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ console.log("app.js file for node");
 console.log(new Date());
 console.log("Added Line 3");
 console.log("edited in browser");
-
+const app=require('express');
+app.length('/',(req,res)=>{
+    res.json({message:"server running"});
+});
+app.listen(700,()=>{
+    console.log("server running on 7000");
+});
